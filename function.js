7b99c9cf6806f06bd8ecd5480860599839d6ab48
AsrieltheGoat <airdroid271@gmail.com>
2021-08-26 08:50:24 -0700

    26/8/2021

--- function.js
+++ function.js
@@ -1,40 +1,41 @@
 // Assign letter
-const KEYS_TOP = ['q', 'w', 'e', 'r', 't', 'y', 'u']
-const KEYS_MIDDLE = ['a', 's', 'd', 'f', 'g', 'h', 'j']
-const KEYS_BOTTOM = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const KEYS_TOP = ['q', 'w', 'e', 'r', 't', 'y', 'u'];
+const KEYS_MIDDLE = ['a', 's', 'd', 'f', 'g', 'h'];
+const KEYS_BOTTOM = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
 const keys = document.querySelectorAll('.key')
-const topKeys = document.querySelectorAll('.key top')
-const middleKeys = document.querySelectorAll('.key middle')
-const bottomKeys = document.querySelectorAll('.key bottom')
+// class moment.
+const topKeys = document.querySelectorAll('.key.top');
+const middleKeys = document.querySelectorAll('.key.middle');
+const bottomKeys = document.querySelectorAll('.key.bottom');
 
-// Allow to click the div
+// Allow to press the notes
 keys.forEach(key => {
-    key.addEventListener('click', () => playNote(key))
+    key.addEventListener('click', () => playNote(key));
 })
 
 // Allow to use keyboard
 document.addEventListener('keydown', e => {
-    if (e.repeat) return
-    const key = e.key
-    const topKeyIndex = KEYS_TOP.indexOf(key)
-    const middleKeyIndex = KEYS_MIDDLE.indexOf(key)
-    const bottomKeyIndex = KEYS_BOTTOM.indexOf(key)
+    if (e.repeat) return;
+    const key = e.key;
+    const topKeyIndex = KEYS_TOP.indexOf(key);
+    const middleKeyIndex = KEYS_MIDDLE.indexOf(key);
+    const bottomKeyIndex = KEYS_BOTTOM.indexOf(key);
 
-    if (topKeyIndex > -1) playNote(topKeys[topKeyIndex])
-    if (middleKeyIndex > -1) playNote(middleKeys[middleKeyIndex])
-    if (bottomKeyIndex > -1) playNote(bottomKeys[bottomKeyIndex])
-})
+    if (topKeyIndex > -1) playNote(topKeys[topKeyIndex]);
+    if (middleKeyIndex > -1) playNote(middleKeys[middleKeyIndex]);
+    if (bottomKeyIndex > -1) playNote(bottomKeys[bottomKeyIndex]);
+});
 
 function playNote(key) {
-    const noteAudio = document.getElementById(key.dataset.note)
-    noteAudio.currentTime = 0
-    noteAudio.play()
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currentTime = 0;
+    noteAudio.play();
 
     // Add press element
-    key.classList.add('active')
+    key.classList.add('active');
     // Remove active element after audio file end
     noteAudio.addEventListener('ended', () => {
-        key.classList.remove('active')
-    })
-}
+        key.classList.remove('active');
+    });
+};
